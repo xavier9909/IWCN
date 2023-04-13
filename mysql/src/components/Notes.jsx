f7e8f57513b841718eb8a328c2c34846b51381a6
xavier9909 <57476138+xavier9909@users.noreply.github.com>
2023-04-13 12:35:44 +0530

    Add files via upload

---
+++ mysql/src/components/Notes.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import {  Heading, Input, Text } from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, CardFooter, SimpleGrid  } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons'
+
+export const Notes = () => {
+    const [list, setList]  = useState([]);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        if(e.key === 'Enter' && e.target.value.trim() !== ''){
+            setList([...list, e.target.value]);
+            e.target.value = '';
+        }
+    };
+
+    const handleDelete = (e) => {
+       let index = list.indexOf(e);
+       if(index > -1){
+        setList((item) => item.filter(ele => ele !== e));
+       }
+    }
+
+    return <div style={{backgroundColor:'#33b099', height:'100vh'}}>
+        <div className="navbar" style={{backgroundColor:'black', height:'50px', color:'white'}}>
+            <strong>
+                <h1 style={{fontSize:'30px'}}>Notes</h1>
+            </strong>
+        </div>
+        <Input m={20} htmlSize={40} width='100' height='50' backgroundColor='white' placeholder="Enter Notes" onKeyUp={handleChange}/>
+       
+            <SimpleGrid spacing={4} mr={20} ml={20} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+        {list.map((e, i) => (
+                <Card mb={10} key={i}>
+                    <CardHeader>
+                    <Heading size='md'> {e}</Heading>
+                    </CardHeader>
+                    <CardBody>
+                    <Text>View a summary of all your note over here.</Text>
+                    </CardBody>
+                    <CardFooter flexDirection='row-reverse'>
+                        <DeleteIcon onClick={() => handleDelete(e)}/>
+                    </CardFooter>
+                </Card>
+        ))}
+        </SimpleGrid>
+    </div>
+}
