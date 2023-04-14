b33ab24d65f939b7ffc3e7f904c3396599fb3d4d
xavier9909 <sm798207@gmail.com>
2023-04-14 14:22:37 +0530

    excel

--- src/index.js
+++ src/index.js
@@ -1,57 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = document.querySelector("#root");
-
-// const handleEdit = (value) => {
-//   const text = document.getElementsByTagName("td").textContent;
-//   console.log("Text", text);
-//   console.log("Value", value);
-//   var row = document.getElementsByClassName("tableee").rows;
-//   console.log("Row", row);
-// };
-
-let dataArr = [
-  "MONTH 1",
-  "Onboarding Call",
-  "Google Search Console",
-  "Website Access",
-  "Tehcnical Audit",
-  "Anchor Text and Sementic Analysis",
-  "Computer Analysis",
-  "Anchor Text/ Url Mapping",
-  "Google Data Studio Report + Local Report Suite",
-  "Site Level Optimization",
-  "On Page Optimization",
-  "Content Creation",
-  "Content Publication",
-  "Premium Press Release",
-  "Authority Niche Palcements",
-  "Review Management",
-  "Index Links",
-  "Video Recap",
-];
-
-ReactDOM.render(
-  <div>
-    <h1 className="redtext">Data Table</h1>
-    <table border={"1px"} className="tableee">
-      <tbody>
-        {dataArr.map((ele, index) => {
-          return (
-            <tr key={index}>
-              <td contentEditable>{ele}</td>
-              <td contentEditable></td>
-              <td contentEditable></td>
-              <td contentEditable></td>
-              <td contentEditable></td>
-              <td contentEditable></td>
-              <td contentEditable></td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  </div>,
-  root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
